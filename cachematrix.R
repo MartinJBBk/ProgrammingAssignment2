c9426848ab4036adfeaf05b2e1a21556d917c18f
MartinJBBK <martinjb@yahoo.com>
2015-07-26 10:30:49 -0400

    Completed assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve are intended to calculate and cache 
+## a matrix and its inverse
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list containing four functions:
+##     set - inserts the matrix to be inverted into x and sets inv to NULL
+##     get - returns x
+##     setInv - sets inv to the calculated inverse of x
+##     getInv - gets inv
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = numeric()) {
+    ## sets up the list that stores matrix x and its inverse
 
+    inv <- NULL
+    # if called, populates the matrix and resets the inverse
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+
+    ## setup the list returned by the function
+    get <- function() x
+    setInv <- function(inverse) inv <<- inverse
+    getInv <- function() inv
+    list(set = set, get = get,
+         setInv = setInv,
+         getInv = getInv)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve returns the inverse of the matrix in the list created by 
+## makeCacheMatrix 
+##
+##     if the inverse has already been calculated and cached (i.e. getInv
+##     does not return null), it returns the value retrieved by getInv
+##
+##     if the inverse has not been cached, it gets the data from the list,
+##     calculates the inverse, stores it in inv and returns the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # calculates or returns the stored inverse of matrix
+    
+    iCalc <- x$getInv()
+    # if the inverse is cached returns the cached value
+    if(!is.null(iCalc)) {
+        message("getting cached data")
+        return(iCalc)
+    }
+
+    # otherwise, calculates, stores and returs inverse
+    data <- x$get()
+    iCalc <- solve(data, ...)
+    x$setInv(iCalc)
+    iCalc
 }
